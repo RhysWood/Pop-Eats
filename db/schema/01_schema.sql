f99c51c9280a99bab9c411b43e888cca28c2f2d8
brysonbest <brycekalden@gmail.com>
2022-01-31 19:54:23 -0800

    Fix merge deletion error.

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -6,9 +6,26 @@ CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" varchar(50) NOT NULL,
   "email" varchar(60) NOT NULL,
-  "password" varchar(50) NOT NULL,
+  "password" varchar(60) NOT NULL,
   "phone_number" bigint NOT NULL,
   "is_owner" boolean NOT NULL
 );
 
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "item_id" INTEGER NOT NULL,
+  "quantity" INTEGER NOT NULL
+);
 
+CREATE TABLE "items" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar(50),
+  "description" varchar(250),
+  "price" INTEGER NOT NULL,
+  "rating" INTEGER(5) NOT NULL
+);
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "items" ADD FOREIGN KEY ("id") REFERENCES "orders" ("item_id");
