4b3bb41707bab5ff614cf84f7aaef09d690e7d43
brysonbest <brycekalden@gmail.com>
2022-02-01 13:22:00 -0800

    Refactor to modular function.

--- services/twilio.js
+++ services/twilio.js
@@ -1,41 +1,23 @@
-const {accountID, authToken} = require('./acctID');
-
-const {db} = require('../db');
+const {accountID, authToken, messagingID} = require('./acctID');
 
 const twilio = require('twilio');
 const client = new twilio(accountID, authToken);
 
-const sendMessage = function(id, message) {
-  return db
-  .query(`
-  SELECT phone_number
-  FROM users
-  WHERE id = $1
-  `, [id])
-  .then((result) => {
-    return client.messages
-      .create({
-        body: message,
-        messagingServiceSid: 'MGed55daed1e16f5639267fce30c5ac8ab',
-        to: result.rows[0]
-      })
-      .then(message => console.log(message.sid))
-      .done();
-
-  })
-  .catch((err) => {
-    console.log(err.message);
-    return null;
-  });
+const sendMessage = function(phoneNumber, message) {
+  return client.messages
+    .create({
+      body: message,
+      messagingServiceSid: messagingID,
+      to: `+${phoneNumber}`
+    })
+    .then(message => console.log(message.sid))
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    })
+    .done();
 };
 
-// client.messages
-//       .create({
-//          body: 'Testing take 2?!',
-//          messagingServiceSid: 'MGed55daed1e16f5639267fce30c5ac8ab',
-//          to: '+17783586873'
-//        })
-//       .then(message => console.log(message.sid))
-//       .done();
-
 module.exports = {sendMessage};
+
+//console.log(sendMessage('17783586873', 'Hello from trial run three!'));
