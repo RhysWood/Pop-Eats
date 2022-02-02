22a94d29e5b0b6fed11342a56bc13a2ea30502a6
RhysWood <rhyswoody@gmail.com>
2022-02-02 04:39:45 +0000

    update ERD schema to include join table

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,30 +1,41 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 DROP TABLE IF EXISTS items CASCADE;
-
-CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "name" varchar(250) NOT NULL,
-  "email" varchar(250) NOT NULL,
-  "password" varchar(250) NOT NULL,
-  "phone_number" bigint NOT NULL,
-  "is_owner" boolean NOT NULL
-);
-
-CREATE TABLE "orders" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "user_id" INTEGER NOT NULL,
-  "item_id" INTEGER NOT NULL,
-  "quantity" INTEGER NOT NULL
-);
+DROP TABLE IF EXISTS orders_items CASCADE;
 
 CREATE TABLE "items" (
   "id" SERIAL PRIMARY KEY,
-  "title" varchar(250),
+  "title" varchar(50),
   "description" varchar(250),
-  "price"  DECIMAL NOT NULL,
-  "rating" INTEGER NOT NULL
+  "price" decimal,
+  "rating" int
 );
 
-ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES users("id");
-ALTER TABLE "orders" ADD FOREIGN KEY ("item_id") REFERENCES items("id");
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar(250),
+  "email" varchar(250),
+  "password" varchar(250),
+  "phone_number" bigint,
+  "is_owner" boolean
+);
+
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" int,
+  "submitted" boolean,
+  "start_date" date,
+  "end_date" date
+);
+
+CREATE TABLE "orders_items" (
+  "id" int,
+  "item_id" int,
+  "order_id" int,
+  "quantity" int
+);
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "orders_items" ADD FOREIGN KEY ("item_id") REFERENCES "items" ("id");
+ALTER TABLE "orders_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
