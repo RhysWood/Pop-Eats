6137b334c2b241fa03235567cee761351d2061d2
brysonbest <brycekalden@gmail.com>
2022-02-03 10:48:02 -0800

    Add template variable using findUser query.

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
+const {findUser} = require('../database');
+
 
 router.get('/:id', (req, res) => {
   console.log("Hello");
   req.session.user_id = req.params.id;
+  const templateVars = {user: findUser(req.session.user_id).name};
   //aquery the datbase based on the id
   //send the found user as a template variable to the page
   res.redirect('/');
