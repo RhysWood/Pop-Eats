4d1e314f97262b99e833a83da47c46731340be7f
RhysWood <rhyswoody@gmail.com>
2022-02-03 02:55:50 +0000

    add login

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router  = express.Router();
+
+
+router.get('/:id', (req, res) => {
+  console.log("Hello");
+  req.session.user_id = req.params.id;
+  //aquery the datbase based on the id
+  //send the found user as a template variable to the page
+  res.redirect('/');
+});
+
+module.exports = router;
