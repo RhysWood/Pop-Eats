8a18b9878dfe584c8149cc76260759bc3be4365a
brysonbest <brycekalden@gmail.com>
2022-02-02 18:24:40 -0800

    Add order total query and list of all order items query.

--- database.js
+++ database.js
@@ -1,13 +1,22 @@
 const {db} = require('./dbpool');
 // const db = require('./server')
 
-const testFunc = () => {
-  const queryString = `SELECT * FROM users WHERE id = $1`;
-  const values = [4];
+//returns user from database
+const findUser = (userID) => {
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1`;
+  const values = [userID];
   return db.query(queryString, values)
    .then((res) => {
-     console.log('hi');
-     console.log(res.rows[0].id)
+     if(res.rows[0]) {
+      console.log('foundUser!');
+      console.log(res.rows[0])
+      return res.rows[0];
+     };
+     console.log('User Not Found');
+     return null;
    })
    .catch((err) => {
     console.log(err.message);
@@ -15,7 +24,6 @@ const testFunc = () => {
   })
 }
 
-testFunc();
 
 //Return all items on menu as array
 const menuItems = () => {
@@ -40,7 +48,7 @@ const allUsers = () => {
   const queryString = `
   SELECT * from users
   GROUP BY id
-  ORDER BY id;;
+  ORDER BY id;
   `;
   return db.query(queryString)
   .then((res) => {
@@ -53,3 +61,58 @@ const allUsers = () => {
   })
 };
 
+//returns all items in an order
+const orderItems = (orderID) => {
+  const queryString = `
+  SELECT items.title, items.price, orders_items.quantity
+  FROM items
+  JOIN orders_items on orders_items.item_id = items.id
+  JOIN orders on orders.id = orders_items.order_id
+  WHERE orders.id = $1
+  GROUP BY items.title, items.price, orders_items.quantity;
+  `;
+  const values = [orderID];
+  return db.query(queryString, values)
+  .then((res) => {
+    if(res.rows[0]) {
+      // console.log('Order Items:');
+      // console.log(res.rows);
+      return res.rows;
+    }
+    console.log(`Order #${orderID} Empty`);
+    return null;
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return null;
+  })
+};
+
+
+//returns total sum of all items in an order as an object
+const orderCost = (orderID) => {
+  const queryString = `
+  SELECT sum(orders_items.quantity * items.price) from orders_items JOIN orders on orders.id = order_id JOIN items on item_id = items.id  WHERE orders.id = $1;
+  `;
+  const values = [orderID];
+  return db.query(queryString, values)
+  .then((res) => {
+    if(res.rows[0]['sum'] !== null) {
+      // console.log('Order Total:');
+      //console.log(res.rows[0]);
+      return res.rows[0];
+    }
+    console.log(`Order #${orderID} Empty`);
+    return null;
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return null;
+  })
+};
+
+
+
+
+
+module.exports = {findUser, menuItems, allUsers, orderItems, orderCost};
