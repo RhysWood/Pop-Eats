674cde247f018892545a54eb7fdee7fa11c463e1
brysonbest <brycekalden@gmail.com>
2022-02-04 12:51:08 -0800

    Add additional lines to orders and order_items for testing.

--- db/seeds/04_orders_items.sql
+++ db/seeds/04_orders_items.sql
@@ -1,5 +1,7 @@
--- INSERT INTO orders_items (item_id, order_id, quantity) VALUES
--- (5, 1, 1),
--- (4, 2, 2),
--- (6, 1, 2),
--- (1, 1, 1)
+INSERT INTO orders_items (item_id, order_id, quantity) VALUES
+(5, 1, 1),
+(6, 1, 4),
+(3, 1, 2),
+(4, 2, 2),
+(6, 3, 2),
+(1, 4, 1)
