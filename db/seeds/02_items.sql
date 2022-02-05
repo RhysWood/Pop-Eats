98305781ea0882952b65b637f3e906b6df3cd39b
brysonbest <brycekalden@gmail.com>
2022-02-05 14:48:44 -0800

    Update items.

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,7 +1,7 @@
-INSERT INTO items (title, description, price, rating, img_url, img_alt) VALUES
-('Kevins Famous Chilli', 'The Office. The trick is to undercook the onions', 11.99, 4.0, 'https://www.fromvalerieskitchen.com/wordpress/wp-content/uploads/2021/01/Halftime-Chili-35.jpg', 'chili', true),
+INSERT INTO items (title, description, price, rating, img_url, img_alt, active) VALUES
+('Kevins Famous Chilli', 'The Office. The trick is to undercook the onions', 11.99, 4.0, 'https://www.fromvalerieskitchen.com/wordpress/wp-content/uploads/2021/01/Halftime-Chili-35.jpg', 'chili', TRUE),
 ('Ram-Don', 'Parasite. Steak included', 19.99, 4.9, 'https://i2.wp.com/www.thegluttonlife.com/wp-content/uploads/2020/05/ram-don-jjapaguri-05.jpg?fit=1024%2C683&ssl=1', 'ram-don', true),
-('Baked Ziti with Mozzareall', 'South Park. Crème fraîche available', 15.99, 3.6, 'https://www.averiecooks.com/wp-content/uploads/2015/10/bakedziti-8-735x735.jpg', 'baked ziti', true),
+('Baked Ziti with Mozzareall', 'South Park. Crème fraîche available', 15.99, 3.6, 'https://www.averiecooks.com/wp-content/uploads/2015/10/bakedziti-8-735x735.jpg', 'baked ziti', TRUE),
 ('Monicas Moist Maker', 'Friends. Gravy soaked piece of bread in the middle', 9.99, 3.6, 'https://hips.hearstapps.com/hmg-prod/images/delish-giant-thanksgiving-leftover-sandwich-2-1542644540.jpg', 'sandwich', true),
 ('The Best Burger in New York', 'How I Met Your Mother. You will be coming back', 13.99, 4.5, 'https://media.timeout.com/images/105187765/image.jpg', 'burger', true),
 ('Beef Wellington', 'Hells Kitchen. Gordon Ramsay would approve', 11.99, 4.9, 'https://images.ctfassets.net/3s5io6mnxfqz/VTFplkmqQemCmaQgYEeGU/a70e0c14de212de3444179fee2223121/image1.jpg?fm=jpg&w=900&fl=progressive', 'beef wellington', true),
