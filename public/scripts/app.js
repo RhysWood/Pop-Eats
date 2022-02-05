e71ae62d799f71aca05de43181cb3d76bd755146
lenilsamuel <lenilvarghese@gmail.com>
2022-02-05 12:18:18 -0700

    Add tax and total functionality to cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,13 @@
 $(document).ready(function () {
   const $cart = $(`<aside>
                   <div><i class="fas fa-shopping-cart"> </i></div>
-                  <div> YOUR CART </div>
-                  <div class="cart-description"> item(s) </div>
-                  <div class="total"> ${`SUBTOTAL: `} <div class="cart-total-item"> </div> </div>
-                  <div class="tax"></div>
-                  <div class="subtotal"></div>
+                  <div> YOUR CART: </div>
+                  <br>
+                  <div class="cart-description"> <div class="item-count"></div>item(s) </div>
+                  <div class="total"> SUB-TOTAL: <div class="cart-total-item"> </div> </div>
+                  <div class="tax">TAX: <div class="cart-tax"></div></div>
+                  <br>
+                  <div class="grand-total">TOTAL: <div class="cart-grand-total"></div></div>
                 </aside>`);
 
   $(".add-btn").on("click", function () {
@@ -77,6 +79,7 @@ $(document).ready(function () {
   // }
 
   updateTotal = (quantityInput) => {
+    let taxRate = 0.12;
     let row = $(quantityInput).parent().parent().parent().parent();
     let price = row.children("#whatever").children(".price").text();
     let quantity = $(quantityInput).val();
@@ -85,30 +88,25 @@ $(document).ready(function () {
     row.children("#total").html(rowPrice.toFixed(2));
 
     let subTotal = 0;
+    let itemTotal = 0;
     $(".table-row").each(function () {
       subTotal += Number($(this).children("#total").text());
+      itemTotal += Number($(this).children(".row-input").children().children().children().val());
     });
 
     let total = subTotal.toFixed(2);
     let totalText = `$ ${total.toString()}`;
-    // console.log(total);
-    // console.log(subTotal.toFixed(2));
+
+    let taxAmt = (taxRate * total).toFixed(2);
+    let taxAmtText = `$ ${taxAmt.toString()}`
+
+    let grandTotal = (Number(total) + Number(taxAmt)).toFixed(2);
+    let grandTotalText = `$ ${grandTotal.toString()}`
+
     $(".cart-total-item").text(totalText);
-    // $(".menu-container").append($cart);
-    // let test = row.children().each(function () {
-    //   // console.log($('#total').text());
-    //   let val = Number(row.children('#total').text());
-    //   // console.log(val);
-    //   subTotal += val;
-    // })
+    $('.cart-tax').text(taxAmtText);
+    $('.cart-grand-total').text(grandTotalText);
+    $('.item-count').text(itemTotal)
 
-    // console.log(subTotal);
-
-    // $("#table-row").each(function () {
-    //   subTotal += Number($(this).children("#total").text());
-
-    // });
-
-    // console.log(subtotal);
   };
 });
