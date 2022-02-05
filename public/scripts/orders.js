1f9354dbeadbceb5646430b50c91dfd1039c2dc6
brysonbest <brycekalden@gmail.com>
2022-02-05 12:50:36 -0800

    Update formatting of orders page with details.

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -14,27 +14,27 @@ $(document).ready(function() {
   });
 
     // button that shows/hides order details
-    const detailsButton = document.getElementsByClassName('tablerow');
-    for (let i=0; i < detailsButton.length; i++) {
-      detailsButton[i].addEventListener('click', function() {
-        if ($(`.order-details${i}`).is(":visible")) {
-          $(`.order-details${i}`).slideUp();
-        } else {
-          $(`.order-details${i}`).slideDown();
-        }
-      });
-    }
+    // const detailsButton = document.getElementsByClassName('tablerow');
+    // for (let i=0; i < detailsButton.length; i++) {
+    //   detailsButton[i].addEventListener('click', function() {
+    //     if ($(`.order-details${i}`).is(":visible")) {
+    //       $(`.order-details${i}`).slideUp();
+    //     } else {
+    //       $(`.order-details${i}`).slideDown();
+    //     }
+    //   });
+    // }
 
   //hides all orders, except for open orders, and all details
   const showOnlyOpen = function() {
     if($('.order-end').is((":visible"))) {
       $('.order-end').parent().slideUp();
     };
-    for (let i=0; i < detailsButton.length; i++) {
-      console.log('testing hiding details');
-      console.log($(`.order-details${i}`).text());
-      $(`.order-details${i}`).slideUp();
-    }
+    // for (let i=0; i < detailsButton.length; i++) {
+    //   console.log('testing hiding details');
+    //   console.log($(`.order-details${i}`).text());
+    //   $(`.order-details${i}`).slideUp();
+    // }
   };
 
   $(() => {
