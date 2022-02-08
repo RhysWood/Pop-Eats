09e596dc4afb2f0c5f4c6c0c463565d2a0dc6522
brysonbest <brycekalden@gmail.com>
2022-02-07 17:55:00 -0800

    Add styling to orders/management loading.

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -5,41 +5,7 @@ $(document).ready(function() {
   const toggleOrders = document.getElementById('toggleOrders');
 
   toggleOrders.addEventListener('click', function() {
-    if($('.order-end').is((":visible"))) {
-      $('.order-end').parent().slideUp();
-    } else {
-      $('.order-end').parent().slideDown();
-    }
-      //document.getElementById('order-details').focus();
+    $('.order-end').parent().toggleClass("done-row");
   });
-
-    // button that shows/hides order details
-    // const detailsButton = document.getElementsByClassName('tablerow');
-    // for (let i=0; i < detailsButton.length; i++) {
-    //   detailsButton[i].addEventListener('click', function() {
-    //     if ($(`.order-details${i}`).is(":visible")) {
-    //       $(`.order-details${i}`).slideUp();
-    //     } else {
-    //       $(`.order-details${i}`).slideDown();
-    //     }
-    //   });
-    // }
-
-  //hides all orders, except for open orders, and all details
-  const showOnlyOpen = function() {
-    if($('.order-end').is((":visible"))) {
-      $('.order-end').parent().slideUp();
-    };
-    // for (let i=0; i < detailsButton.length; i++) {
-    //   console.log('testing hiding details');
-    //   console.log($(`.order-details${i}`).text());
-    //   $(`.order-details${i}`).slideUp();
-    // }
-  };
-
-  $(() => {
-    showOnlyOpen();
-  });
-
 
 });
